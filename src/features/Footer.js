469a0706fc931aea50156b30fe750f38af68cfe1
Craig Kelleher <kelleher.craig@gmail.com>
2023-04-04 13:34:25 -0700

    Added Footer For each page

---
+++ src/features/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <p>Created by Craig, Estevan, Jeremy, Leslie, Robert, Robin</p>
+        </footer>
+    )
+}
+
+export default Footer;
