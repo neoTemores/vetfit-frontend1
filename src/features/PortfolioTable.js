44e6cf9d75665e516f3299bab5d6be9a0581de10
Craig Kelleher <kelleher.craig@gmail.com>
2023-04-05 09:10:16 -0700

    Handle buying and selling of shares

--- src/features/PortfolioTable.js
+++ src/features/PortfolioTable.js
@@ -1,26 +1,39 @@
 import React, {useState} from "react";
 import '../PortfolioPage.css';
+import Navbar from "../Components/Navbar";
 
 function PortfolioTable(props) {
     const [portfolio, setPortfolio] = useState(props.portfolio);
+    const [sharesToBuyOrSell, setSharesToBuyOrSell] = useState({});
     
-    function handleBuy(symbol) {
-        const sharePrice = getSharePrice(symbol);
-        const numberOfShares = prompt("How many shares of ${symbol} would you like to buy?")
-    // TODO: add code to update portfolio when you buy a share
-    }
+    function handleBuyShares(event, symbol) {
+        const value = event.target.value;
+        if (window.confirm("Do you really want to purchase the shares?")) {
+            setSharesToBuyOrSell((prevShares) => ({
+                ...prevShares, [symbol]: value ? parseInt(value) : undefined,
+            }));
+            // TODO: connect code to update portfolio when you buy a share
+        }
+    };
 
-    function handleSell(symbol) {
-        const sharePrice = getSharePrice(symbol);
-        const numberOfShares = prompt("How many shares of ${symbol} would you like to sell?")
-    // TODO: add code to update portfolio when you sell a share
-    }
+    function handleSellShares(event, symbol) {
+        const value = event.target.value;
+        if (window.confirm("Do you really want to sell the shares?")) {
+            setSharesToBuyOrSell((prevShares) => ({
+                ...prevShares, [symbol]: value ? parseInt(value) : undefined,
+            }));
+            // TODO: connect code to update portfolio when you sell a share
+        }
+    };
 
     function getSharePrice(symbol) {
+        // TODO: Connect to finnhubAPI
         return 150.00; //for testing
     }
 
     return (
+        <>
+        <Navbar />
         <table>
             <tr>
                 <th>Symbol</th>
@@ -39,13 +52,18 @@ function PortfolioTable(props) {
                     <td>${stock.amountInvested}</td>
                     <td>${getSharePrice(stock.symbol) * stock.numberOfShares}</td>
                     <td>
-                        <button onClick={() => handleBuy=(stock.symbol)}>Buy</button>
-                        <button onClick={() => handleSell=(stock.symbol)}>Sell</button>
+                        <button onClick={handleBuyShares}>Buy</button>
+                    <label>
+                        <input type="number" min="0" value={sharesToBuyOrSell[stock.symbol]} onChange={(event) => sharesToBuyOrSell(event.target.value)} />
+                    </label>
+                    
+                    <button onClick={handleSellShares}>Sell</button>
                     </td>
                 </tr>
                 )}
         </tbody>
         </table>
+        </>
     );
 }
 
